f56edb62f984e2a00e8ad2152297631b81fbfebb
Juwan H. <63868021+Juwan-Hollingsworth@users.noreply.github.com>
2025-09-26 21:27:35 -0400

    Update mega menu with Solutions structure and create routes

---
+++ src/app/solutions/landing-page-waitlist/page.tsx
@@ -0,0 +1,38 @@
+import Contents from '@/components/our-serices-details/Contents';
+import CTAV1 from '@/components/shared/cta/CTAV1';
+import FooterThree from '@/components/shared/footer/FooterThree';
+import NavbarOne from '@/components/shared/header/NavbarOne';
+import PageHero from '@/components/shared/PageHero';
+import { Metadata } from 'next';
+import { Fragment } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Landing Page + Waitlist - NextSaaS',
+};
+
+const LandingPageWaitlist = () => {
+  return (
+    <Fragment>
+      <NavbarOne
+        className="bg-white/60 backdrop-blur-[25px] dark:border dark:border-stroke-7 dark:bg-background-7"
+        btnClassName="btn-secondary hover:btn-primary dark:btn-accent"
+      />
+      <main className="bg-background-3 dark:bg-background-7">
+        <PageHero title="Landing Page + Waitlist" heading="Landing Page + Waitlist" link="/solutions/landing-page-waitlist" />
+        <Contents slug="payment-solutions" />
+        <CTAV1
+          className="dark:bg-background-6 bg-white"
+          badgeClass="hidden"
+          ctaHeading="Build a complete website using the"
+          spanText="assistance"
+          description="Start your free trial today and see your ideas come to life easily and creatively."
+          btnClass="hover:btn-secondary dark:hover:btn-accent"
+          ctaBtnText="Get started"
+        />
+      </main>
+      <FooterThree />
+    </Fragment>
+  );
+};
+
+export default LandingPageWaitlist;
---
+++ src/app/solutions/no-code-mvp-development/page.tsx
@@ -0,0 +1,38 @@
+import Contents from '@/components/our-serices-details/Contents';
+import CTAV1 from '@/components/shared/cta/CTAV1';
+import FooterThree from '@/components/shared/footer/FooterThree';
+import NavbarOne from '@/components/shared/header/NavbarOne';
+import PageHero from '@/components/shared/PageHero';
+import { Metadata } from 'next';
+import { Fragment } from 'react';
+
+export const metadata: Metadata = {
+  title: 'No-Code MVP Development - NextSaaS',
+};
+
+const NoCodeMvpDevelopment = () => {
+  return (
+    <Fragment>
+      <NavbarOne
+        className="bg-white/60 backdrop-blur-[25px] dark:border dark:border-stroke-7 dark:bg-background-7"
+        btnClassName="btn-secondary hover:btn-primary dark:btn-accent"
+      />
+      <main className="bg-background-3 dark:bg-background-7">
+        <PageHero title="No-Code MVP Development" heading="No-Code MVP Development" link="/solutions/no-code-mvp-development" />
+        <Contents slug="trading-platforms" />
+        <CTAV1
+          className="dark:bg-background-6 bg-white"
+          badgeClass="hidden"
+          ctaHeading="Build a complete website using the"
+          spanText="assistance"
+          description="Start your free trial today and see your ideas come to life easily and creatively."
+          btnClass="hover:btn-secondary dark:hover:btn-accent"
+          ctaBtnText="Get started"
+        />
+      </main>
+      <FooterThree />
+    </Fragment>
+  );
+};
+
+export default NoCodeMvpDevelopment;
